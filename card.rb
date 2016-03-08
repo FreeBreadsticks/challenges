9bfb3337aeda1f10afc556e80718d5af50d3fa64
Erika <erika.hughes@dbspublishing.com>
2016-03-08 19:46:22 +0000

    Complete the OOP deck of cards challenge

--- card.rb
+++ card.rb
@@ -18,23 +18,37 @@ end
 class Deck
  
   def initialize
+
     @cards = []
-    @cards << Card.new(10, :spades)
-    @cards << Card.new(9, :diamonds)
+    @ranks = [2,3,4,5,6,7,8,9,10,:Jack,:Queen,:King,:Ace]
+    @suits = [:Spades, :Diamonds, :Hearts, :Clubs]
+
+    @ranks.each do |rank|
+      @suits.each do |suit|
+        @cards << Card.new(rank, suit)
+      end
+    end   
+    
+  end
+
+  def out_put
+    @cards.each do |card|
+      card.output_card
+    end
   end
 
   def shuffle
     @cards.shuffle!
   end
 
-  def output
-    @cards.each do |card|
-      card.output_card
-    end
+  def deal
+    top = @cards.shift
+    puts "#{top.output_card} is the top card of the deck."
   end
 
 end
 
 deck = Deck.new
 deck.shuffle
-deck.output
+#deck.out_put
+deck.deal
