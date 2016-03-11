0d4ff577b7e1780226e1cf9315498c9744287de0
Erika <erika.hughes@dbspublishing.com>
2016-03-11 22:00:34 +0000

    Refactored code. added loop

--- blur1.rb
+++ blur1.rb
@@ -7,14 +7,17 @@ class Image
   end
 
   def output_image
-    row1 = img[0]
-    row2 = img[1]
-    row3 = img[2]
-    row4 = img[3]
-    puts "#{row1.join}"
-    puts "#{row2.join}"
-    puts "#{row3.join}"
-    puts "#{row4.join}"
+    img.each_index do |i| 
+      puts "#{img[i].join}"
+    end
+    #row1 = img[0]
+    #row2 = img[1]
+    #row3 = img[2]
+    #row4 = img[3]
+    #puts "#{row1.join}"
+    #puts "#{row2.join}"
+    #puts "#{row3.join}"
+    #puts "#{row4.join}"
     end
 
   end
